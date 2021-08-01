783e95ae1cf0146217ec80363a1854c892022987
tcs-kiran <63388243+tcs-kiran@users.noreply.github.com>
2021-08-01 16:05:11 +0530

    Create 1553A.cpp

---
+++ Codeforces/Harbour space scholarship (Prblm no. 1553)/1553A.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        cout << (n+1)/10 << endl;
+    }
+    return 0;
+}
