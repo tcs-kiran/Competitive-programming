8d280afbe6af9cca8da8a2bad9465f817deee7b8
tcs-kiran <63388243+tcs-kiran@users.noreply.github.com>
2021-08-05 10:46:45 +0530

    Create 1549A.cpp

---
+++ Codeforces/Prblm no.1549,1548 (Round #736)/1549A.cpp
@@ -0,0 +1,18 @@
+// If we assume P mod a=1 then as P is prime >5 , P mod 2=1, P mod P-1 =1
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll n;
+        cin >> n;
+        cout << "2 " << n-1 << endl;
+    }
+    return 0;
+}
