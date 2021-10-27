93d2f540c9b42843b548c7e5a7fd3081c0029e53
tcskiran <63388243+tcskiran@users.noreply.github.com>
2021-10-27 08:26:36 +0530

    Create 1594A.cpp

---
+++ Codeforces/Prblm no.1594 (Round #747)/1594A.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define MOD 1000000007
+typedef long long ll;
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    ll t;
+    cin >> t;
+    while (t--) {
+        ll i,j,k,n,l,r;
+        cin >> n;
+        cout << -n+1 << " " << n << endl;
+    }
+    return 0;
+}
